f700ed69eca3c6ffa078b914c7703121276bcfb0
Danny Santino <dev@dannysantino.com>
2025-08-13 15:40:46 +0100

    refactor: use port: 5000 for backend server

--- server/server.js
+++ server/server.js
@@ -1,5 +1,5 @@
 import { app } from "./index.js"
 
-const PORT = process.env.PORT || 8080
+const PORT = process.env.PORT || 5000
 
 app.listen(PORT, () => console.log(`Server listening on PORT ${PORT}`));
